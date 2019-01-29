1f4e105a40214cb87dadb56ccad68e6e29cb02ba
tanert <taner.temel@comodo.com>
2019-01-29 13:36:10 +0300

    EDit

---
+++ ms-config-server/src/main/java/com/temelt/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.temelt.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Project microservice-demo
+ * Created by taner on 29.01.2019
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
--- ms-eureka-server/src/main/java/com/temelt/eurekaserver/EurekaServerApplication.java
+++ ms-eureka-server/src/main/java/com/temelt/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -12,7 +14,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
