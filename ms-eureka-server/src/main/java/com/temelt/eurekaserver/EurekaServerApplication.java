6ef7760dd309e8f886a9aa920ded4e8c13441134
tanert <taner.temel@comodo.com>
2019-01-30 17:41:41 +0300

    Eureka-Server Config-Server integration

--- ms-eureka-server/src/main/java/com/temelt/eurekaserver/EurekaServerApplication.java
+++ ms-eureka-server/src/main/java/com/temelt/eurekaserver/EurekaServerApplication.java
@@ -1,11 +1,7 @@
 package com.temelt.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -14,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
